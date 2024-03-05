3f46f248c36d5b54b8d7f3b8973c7ec39165d76d
Rahmansk22 <154653343+Rahmansk22@users.noreply.github.com>
2024-03-05 14:11:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 // Typing animation effect
 var typed = new Typed(".effect",{
-    strings:[
-        "Developer","Blogger", "Designer"
-    ],
+    strings:[ "Developer","Blogger", "Designer" ],
     typeSpeed: 60,
     backSpeed: 40,
-    loop: true
-});
+    loop: true });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth' });
     });
 });
