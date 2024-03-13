9add13d83443bf33288c2c9297b53c9130e34c50
Rahmansk22 <154653343+Rahmansk22@users.noreply.github.com>
2024-03-13 19:53:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// Typing animation effect
 var typed = new Typed(".effect",{
     strings:[ "Developer","Blogger", "Designer" ],
     typeSpeed: 60,
     backSpeed: 40,
     loop: true });
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth' });
-    });
-});
